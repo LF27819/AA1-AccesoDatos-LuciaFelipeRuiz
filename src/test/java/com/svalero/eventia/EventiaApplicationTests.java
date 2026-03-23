a9859036e563a8c1ffde8f8a560f3cd14442a3c0
LF27819 <luciafeliperuiz@gmail.com>
2026-03-23 19:53:49 +0100

    Estructura Spring Boot

---
+++ src/test/java/com/svalero/eventia/EventiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.eventia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
